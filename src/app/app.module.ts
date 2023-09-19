5ce6479c3b7e8774df7b25583c73fbed39d7ba4c
0jimah <godfreyakuba@gmail.com>
2023-09-19 11:50:45 +0200

    Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpLink } from 'apollo-angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { SignUpModule } from './auth/sign-up/sign-up.module';
+import { LoginModule } from './auth/login/login.module';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeModule } from './home/home.module';
+import { BookmarkModule } from './bookmarks/bookmark/bookmark.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ApolloModule,
+    AppRoutingModule,
+    HeaderModule,
+    SignUpModule,
+    LoginModule,
+    HomeModule,
+    BookmarkModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'api/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
